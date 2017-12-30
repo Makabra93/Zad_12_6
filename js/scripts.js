124c8a6bbf8193f5f5b62fd0aad618f4a818f805
Mateusz Lawczys <ma6tek@gmail.com>
2017-12-31 00:17:03 +0100

    add js and styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,34 @@
 $(function() {
+    var url = 'https://restcountries.eu/rest/v1/name/';
+    var countriesList = $('#countries');
 
+    $("#country-name").focus(function() {
+        $(this).data("hasfocus", true);
+    });
+    $("#country-name").blur(function() {
+        $(this).data("hasfocus", false);
+    });
+    $(document.body).keyup(function(ev) {
+        if (ev.which === 13 && $("#country-name").data("hasfocus")) {
+            searchCountries();
+        }
+    });
+    $('#search').click(searchCountries);
+
+    function searchCountries() {
+        var countryName = $('#country-name').val();
+        if (!countryName.length) countryName = 'Poland';
+        $.ajax({
+            url: url + countryName,
+            method: 'GET',
+            success: showCountriesList
+        });
+    function showCountriesList(resp) {
+        countriesList.empty();
+        resp.forEach(function(item) {
+            $('<li>').text(item.name).appendTo(countriesList);
+        });
+    }
+  }
 
 });
