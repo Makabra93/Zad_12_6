bbb12e8f5dffe822a0e12634495e3474790c7f81
Mateusz Lawczys <ma6tek@gmail.com>
2018-01-02 22:11:03 +0100

    Change js

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,16 @@ $(function() {
     function searchCountries() {
         var countryName = $('#country-name').val();
         if (!countryName.length) countryName = 'Poland';
-        $.ajax({
-            url: url + countryName,
-            method: 'GET',
-            success: showCountriesList
-        });
-    function showCountriesList(resp) {
-        countriesList.empty();
-        resp.forEach(function(item) {
-            $('<li>').text(item.name).appendTo(countriesList);
-        });
+            $.ajax({
+                url: url + countryName,
+                method: 'GET',
+                success: showCountriesList
+            });
+            function showCountriesList(resp) {
+                countriesList.empty();
+                resp.forEach(function(item) {
+                    $('<li>').text(item.name).appendTo(countriesList);
+                });
+            }
     }
-  }
 });
